5237544446ceba22fc29166cf0f6533a49c762bb
ShukshinWl16it <vladimirsuksin85@gmail.com>
2025-10-21 09:31:32 +0500

    Create Time.java

---
+++ Time.java
@@ -0,0 +1,23 @@
+public class Time {
+    private int secunds;
+    public Time(){
+       this.secunds=0;
+    }
+    public Time(int secunds){
+        this.secunds=secunds;
+    }
+    public int getSecunds(){
+        return secunds;
+    }
+    public void setSecunds(int secunds){
+        this.secunds=secunds;
+    }
+    @Override
+    public String toString(){
+        int secondsDay=secunds%(3600*24);
+        int hours=secondsDay/3600;
+        int minutes=(secondsDay%3600)/60;
+        int seconds=secondsDay%60;
+        return hours+":"+minutes+":"+seconds;
+    }
+}
