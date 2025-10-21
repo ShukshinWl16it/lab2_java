b4e473e72df64b43f0b945cadf6bcb03e2c1d2df
ShukshinWl16it <vladimirsuksin85@gmail.com>
2025-10-21 10:38:55 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -17,16 +17,21 @@ public class Main {
             choice=scanner.nextInt();
             switch (choice){
                 case 1:
-                    System.out.println("Пример преобразования 10, 10000, 100000 секунд: ");
-                    Time time1=new Time();
-                    time1.setSecunds(10);
-                    Time time2=new Time();
-                    time2.setSecunds(10000);
-                    Time time3=new Time();
-                    time3.setSecunds(100000);
-                    System.out.println("Первое время: "+time1);
-                    System.out.println("Второе время: "+time2);
-                    System.out.println("Третье время: "+time3);
+                    try{
+                        System.out.println("Пример преобразования 10, 10000, 100000 секунд: ");
+                        Time time1=new Time();
+                        time1.setSecunds(10);
+                        Time time2=new Time();
+                        time2.setSecunds(10000);
+                        Time time3=new Time();
+                        time3.setSecunds(100000);
+                        System.out.println("Первое время: "+time1);
+                        System.out.println("Второе время: "+time2);
+                        System.out.println("Третье время: "+time3);
+                    }catch(IllegalArgumentException e){
+                    System.out.println("Ошибка: "+e.getMessage());
+                }
+
                     break;
                 case 2:
                     System.out.println("Примеры вывода с 1, 5, 23 этажами: ");
@@ -72,26 +77,39 @@ public class Main {
                     Employee.printEmployees(salesDepartment);
                     break;
                 case 5:
-                    House house4=new House(2);
-                    House house5=new House(35);
-                    House house6=new House(91);
-                    System.out.println("Первый дом: "+house4);
-                    System.out.println("Второй дом: "+house5);
-                    System.out.println("Третий дом: "+house6);
-                    house4.setFloors(45);
-                    house5.setFloors(5);
-                    System.out.println("Первый дом: "+house4);
-                    System.out.println("Второй дом: "+house5);
-                    System.out.println("Третий дом: "+house6);
+                    try{
+                        House house4=new House(2);
+                        House house5=new House(35);
+                        House house6=new House(91);
+                        System.out.println("Первый дом: "+house4);
+                        System.out.println("Второй дом: "+house5);
+                        System.out.println("Третий дом: "+house6);
+                        house4.setFloors(45);
+                        house5.setFloors(5);
+                        System.out.println("Первый дом: "+house4);
+                        System.out.println("Второй дом: "+house5);
+                        System.out.println("Третий дом: "+house6);
+                        House house7=new House(-56);
+                        House house8=new House(0);
+                        House house9=new House(1000);
+
+                    }catch(IllegalArgumentException e){
+                        System.out.println("Ошибка: "+e.getMessage());
+                    }
+
                     break;
                 case 6:
-                    Gun gun=new Gun(3);
-                    System.out.println("Количество патронов: "+gun.getCartridges());
-                    for(int i=1;i<6;i++){
-                        System.out.println(i+" выстрел: "+gun);
-                        System.out.println("Оставшееся количество патронов: "+gun.getCartridges());
+                    try{
+                        Gun gun=new Gun(3);
+                        System.out.println("Количество патронов: "+gun.getCartridges());
+                        for(int i=1;i<6;i++){
+                            System.out.println(i+" выстрел: "+gun);
+                            System.out.println("Оставшееся количество патронов: "+gun.getCartridges());
+                        }
+                        break;
+                    }catch(IllegalArgumentException e){
+                        System.out.println("Ошибка: "+e.getMessage());
                     }
-                    break;
             }
         }while(choice!=0);
     }
